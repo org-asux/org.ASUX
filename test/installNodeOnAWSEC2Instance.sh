63cc525c2c03d2f6161b30eb57d2e74eef420908
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-05-04 00:17:25 -0400

    2 b tested soon on AWS instance

--- test/installNodeOnAWSEC2Instance.sh
+++ test/installNodeOnAWSEC2Instance.sh
@@ -4,14 +4,14 @@ echo ''; echo ''; echo ''
 echo .. you must run this command as follows:-
 echo 'NO NEED NO NEED NO NEED NO NEED --> sudo -i'
 echo ''; echo ''; echo ''
-echo '> cd ~'
-echo ''; echo ' VERY IMPORTANT.  VERY IMPORTANT. VERY IMPORTANT.  You must be in your HOME folder'; echo ''
-echo "> ./$0"
+echo "	.   $0"
 read -p "CONFIRM all of the above (#1): "  Variable
 read -p "CONFIRM all of the above (#2): "  Variable
 read -p "CONFIRM all of the above (#3): "  Variable
 
+pushd ~
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 7
+popd
