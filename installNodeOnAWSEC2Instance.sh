176144b85164b702266f557e28714b8f9472e113
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-06-04 19:22:58 -0400

    test/installNodeOnAWSEC2Instance.sh - to - installNodeOnAWSEC2Instance.sh

---
+++ installNodeOnAWSEC2Instance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo ''; echo ''; echo ''
+echo .. you must run this command as follows:-
+echo 'NO NEED NO NEED NO NEED NO NEED --> sudo -i'
+echo ''; echo ''; echo ''
+echo "	.   $0"
+read -p "CONFIRM all of the above (#1): "  Variable
+read -p "CONFIRM all of the above (#2): "  Variable
+read -p "CONFIRM all of the above (#3): "  Variable
+
+pushd ~
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 7
+npm install commander
+
+popd
+
+echo ''
+echo 'please logout and login again.. or .. enter the commands in lines 14 & 15 (in this BASH script) on the terminal'
+
+
