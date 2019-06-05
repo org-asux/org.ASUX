9af77daf9a506fa97cf30d462d1d298169cddb21
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-06-04 20:14:55 -0400

    1st run on AWS failed miserably.  Now I detect whether this is sourced or run as a program.

--- installNodeOnAWSEC2Instance.sh
+++ installNodeOnAWSEC2Instance.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-echo ''; echo ''; echo ''
-echo .. you must run this command as follows:-
-echo 'NO NEED NO NEED NO NEED NO NEED --> sudo -i'
-echo ''; echo ''; echo ''
-echo "	.   $0"
-read -p "CONFIRM all of the above (#1): "  Variable
-read -p "CONFIRM all of the above (#2): "  Variable
-read -p "CONFIRM all of the above (#3): "  Variable
 
-pushd ~
+CmdPath="$0"
+#___ echo $CmdPath
+CmdName=`basename "$CmdPath"`
+#___ echo ${CmdName}
+
+if [ "${CmdName}" == "installNodeOnAWSEC2Instance.sh" ]; then
+
+	echo ''; echo ''; echo ''
+	echo '.. you must run this command as follows:-         (ATTENTION: NO NEED   NO NEED   NO NEED  --> sudo -i)'
+	echo ''
+	echo "	  .   ${CmdName}"
+	echo ''; echo ''; echo ''
+	# read -p "CONFIRM all of the above (#1): "  Variable
+	# read -p "CONFIRM all of the above (#2): "  Variable
+	# read -p "CONFIRM all of the above (#3): "  Variable
+	exit 1
+fi
+
+pushd ~ > /dev/null
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 7
 npm install commander
 
-popd
+popd > /dev/null
 
 echo ''
-echo 'please logout and login again.. or .. enter the commands in lines 14 & 15 (in this BASH script) on the terminal'
+echo 'please logout and login again.. or .. (if you do NOT want to logout, then .. enter the commands in lines 23-26 (in this BASH script) on the terminal'
 
 
