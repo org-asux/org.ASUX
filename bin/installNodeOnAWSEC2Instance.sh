2206ba60aef75dfcb84e49a28711c8d1018aea62
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-07-17 11:44:28 -0400

    now shell-session PATH is changed in bin/installNodeOnAWSEC2Instance.sh, so that user does Not have to logout and login again

--- bin/installNodeOnAWSEC2Instance.sh
+++ bin/installNodeOnAWSEC2Instance.sh
@@ -13,9 +13,9 @@ set SCRIPTFLDR=$PWD
 if [ "${CmdName}" == "installNodeOnAWSEC2Instance.sh" ]; then
 
 	echo ''; echo ''; echo ''
-	echo '.. you must run this command as follows:-         (ATTENTION: NO NEED   NO NEED   NO NEED  --> sudo -i)'
+	>&2 echo '.. you must run this command as follows:-         (ATTENTION: NO NEED   NO NEED   NO NEED  --> sudo -i)'
 	echo ''
-	echo "	  .   ${CmdName}"
+	>&2 echo "	  .   ${CmdName}"
 	echo ''; echo ''; echo ''
 	# read -p "CONFIRM all of the above (#1): "  Variable
 	# read -p "CONFIRM all of the above (#2): "  Variable
@@ -33,14 +33,35 @@ npm install commander
 
 popd > /dev/null
 
-echo ''; echo ''; echo ''
-echo ''; echo ''; echo ''
-echo 'Recommendation: please logout and login again.. or .. (if you do NOT want to logout, then .. enter the commands in lines 27-28 (in this BASH script) on the terminal'
-echo ''; echo ''; echo ''
+###-------------------
+### NOTE: This function will __ALSO__ change the PATH for this shell-session
+### NOTE: This function is DUPLICATED from '${orgASUXFldr}/install'
+addToPathInBashrc()
+{
+	echo ' ' >> ~/.bashrc
+	echo 'export PATH=.:"'${orgASUXFldr}'":${PATH}' >> ~/.bashrc
+	>&2 echo 'Done!'
+
+	export PATH=.:${orgASUXFldr}:${PATH}	### This will also change the PATH for this shell-session
+}
+
+###-------------------
+
+addToPathInBashrc
+
+###-------------------
+>&2 echo ''; echo ''; echo ''
+>&2 echo ''; echo ''; echo ''
+>&2 echo 'A Recommendation: please logout and login again.. or .. (if you are an ADVANCED-user, then .. enter the commands in lines 27-28 (in this BASH script) on the terminal'
+>&2 echo ''; echo ''; echo ''
 sleep 7
-echo ''; echo ''; echo ''
-echo "Remember to rerun "./install" again! (in the directory ${SCRIPTFLDR})"
-echo ''; echo ''; echo ''
+>&2 echo ' '
+>&2 read -p 'If you are OK, "install" command will be re-run again.  Press Cntl-C to STOP, or .. Otherwise press ENTER key to continue' BLACKHOLEVARIABLE
+# >&2 echo ''; echo ''; echo ''
+# >&2 echo "Remember to rerun "./install" again! (in the directory ${SCRIPTFLDR})"
+# >&2 echo ''; echo ''; echo ''
 
+./install
 
+### EoScript
 
